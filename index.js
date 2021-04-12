89ec364e933a25ebecfc626498306fb174fc7031
Brandon Ford <Brandon.ford.617@gmail.com>
2021-04-12 08:28:47 -0400

    npm inquirer from server and added util section and placeholde file

---
+++ index.js
@@ -0,0 +1,19 @@
+// Dependencies and variables
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const generateReadme = require("./utils/generateReadme.js")
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
